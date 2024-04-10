339f7065d03fe1dcc7871729d59489e4d8a27ade
Jiří Žák <jiri.zak89@seznam.cz>
2024-04-10 14:38:24 +0200

    Extension init
    commit includes:
      - initial scaffold of Quasar.js App Extension

---
+++ extension/src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Quasar App Extension index/runner script
+ * (runs on each dev/build)
+ *
+ * Docs: https://quasar.dev/app-extensions/development-guide/index-api
+ */
+
+export default function (api) {
+  //
+  console.log("RUNNING THE EXT index file....");
+
+  console.log(api);
+
+  console.log(".... ENDING ....");
+}
